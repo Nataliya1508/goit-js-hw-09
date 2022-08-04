eaec9ff52a0421dcccad86a53842061a3c0267b6
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-08-04 13:20:19 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', startClick);
+stopBtn.addEventListener('click', stopClick);
+
+let timerId = 0;
+
+stopBtn.disabled = true;
+function startClick() {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    
+     startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopClick() {
+    clearInterval(timerId);
+     startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+}
 
