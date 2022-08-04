a17a16bc445ef76c4aaa14d0d573316e150a0a92
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-08-04 18:54:55 +0300

    task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,80 @@
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const dataStart = document.querySelector('[data-start]');
+const dataDay = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+const dataPicker = document.querySelector('#datetime-picker');
+
+
+
+dataStart.disabled = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    
+      const dateNow = Date.now();
+    //   dataStart.disabled = true;
+      if (selectedDates[0] < dateNow) {
+          
+          alert("Please choose a date in the future")
+          return
+      }
+      const choiceDate = selectedDates[0].getTime()
+
+      dataStart.disabled = false;
+      function timer() {
+          const intrvalid = setInterval(() => {
+              const currentDate = Date.now();
+              const delta = choiceDate - currentDate;
+              console.log(delta)
+              dataStart.disabled = true;
+              if (delta <= 0) {
+                  clearInterval(intrvalid)
+                  return
+              }
+              convertMs(delta)
+          }, 1000)
+                       
+      }
+      dataStart.addEventListener('click', timer)
+  },
+};
+
+flatpickr(dataPicker, options);
+
+
+function convertMs(ms) {
+ 
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+
+
+dataDay.textContent = days;
+dataHours.textContent = hours;
+dataMinutes.textContent = minutes;
+dataSeconds.textContent = seconds;
+
+ return { days, hours, minutes, seconds };
+    
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
